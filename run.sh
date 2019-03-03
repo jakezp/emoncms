1bf7c57ea4e18252620b0dc203ce4518a704ed00
jakezp <jakezp@gmail.com>
2019-03-03 13:36:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Autogenerate MYSQL password if it was not specified
+if [[ -z $MYSQL_PASSWORD ]] && [[ ! -f /home/pi/mysql_passwd ]]; then
+   MYSQL_PASSWORD=$(dd if=/dev/urandom bs=18 count=1 2>/dev/null | base64)
+   echo $MYSQL_PASSWORD > /home/pi/mysql_passwd
+fi
+
 # Create pi user
 id pi > /dev/null 2>&1
 if [[ $? != 0 ]]; then 
@@ -10,34 +16,23 @@ else
  echo -e "\n=> User pi already exists"
 fi
 
-#Install emoncms
-INDEX="/var/www/html/settings.php"
-if [[ ! -d $INDEX ]]; then
-  echo -e "\n=> Installing emoncms"
-  rm -rf /var/www/html
-  rm -rf /home/pi/emailreport
-  rm -rf /home/pi/sync
-  rm -rf /home/pi/emonpi
-  rm -rf /usr/local/bin/emoncms_usefulscripts
-  git clone https://github.com/emoncms/emoncms.git /var/www/html
-  git clone https://github.com/emoncms/app.git /var/www/html/Modules/app
-  git clone https://github.com/emoncms/usefulscripts.git /usr/local/bin/emoncms_usefulscripts
-  git clone https://github.com/emoncms/dashboard.git /var/www/html/Modules/dashboard
-  git clone https://github.com/emoncms/device.git /var/www/html/Modules/device
-  git clone https://github.com/emoncms/graph.git /var/www/html/Modules/graph
-  git clone https://github.com/emoncms/sync.git /home/pi/sync
-  git clone https://github.com/openenergymonitor/emonpi.git /home/pi/emonpi
-  ln -s /home/pi/sync/sync-module /var/www/html/Modules/sync
-  ln -s /var/www/html /var/www/emoncms
-else
-  echo -e "\n=> Using existing emoncms installation"
-fi
-
-# Check that user has supplied a MYSQL_PASSWORD
-if [[ -z $MYSQL_PASSWORD ]]; then
-  echo 'Ensure that you have supplied a password using the -e MYSQL_PASSWORD="mypass"'
-  exit 1;
-fi
+#Install / update emoncms
+echo -e "\n=> Installing emoncms"
+rm -rf /var/www/html
+rm -rf /home/pi/emailreport
+rm -rf /home/pi/sync
+rm -rf /home/pi/emonpi
+rm -rf /usr/local/bin/emoncms_usefulscripts
+git clone https://github.com/emoncms/emoncms.git /var/www/html
+git clone https://github.com/emoncms/app.git /var/www/html/Modules/app
+git clone https://github.com/emoncms/usefulscripts.git /usr/local/bin/emoncms_usefulscripts
+git clone https://github.com/emoncms/dashboard.git /var/www/html/Modules/dashboard
+git clone https://github.com/emoncms/device.git /var/www/html/Modules/device
+git clone https://github.com/emoncms/graph.git /var/www/html/Modules/graph
+git clone https://github.com/emoncms/sync.git /home/pi/sync
+git clone https://github.com/openenergymonitor/emonpi.git /home/pi/emonpi
+ln -s /home/pi/sync/sync-module /var/www/html/Modules/sync
+ln -s /var/www/html /var/www/emoncms
 
 # Prepare mysql
 if [[ ! -f /etc/mysql/my.cnf ]]; then
@@ -50,7 +45,7 @@ if [[ ! -d $MYSQL_HOME/mysql ]]; then
   echo -e "\n=> Installing MySQL ..."
   mysql_install_db > /dev/null 2>&1
   chown -R mysql:mysql /var/lib/mysql/ > /dev/null 2>&1
-  if [[ ! -f /var/run/mysqld ]]; then
+  if [[ ! -d /var/run/mysqld ]]; then
     mkdir -p /var/run/mysqld > /dev/null 2>&1
     chown mysql:mysql /var/run/mysqld > /dev/null 2>&1
   fi
